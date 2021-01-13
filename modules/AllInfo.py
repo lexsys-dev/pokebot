42a410a2303ba61f4679005b907f4881a3a1e9b1
Alexsander Ribeiro <alexmrp@gmail.com>
2021-01-13 18:57:33 +0000

    Módulo parcialmente traduzido para português.

--- modules/AllInfo.py
+++ modules/AllInfo.py
@@ -7,22 +7,22 @@ def get_all_info(api, query):
 	try:
 		pikasp = api.get_pokemon_species(query)
 	except:
-		response = "Invalid pokemon name."
+		response = "Nome inválido."
 		return response
 
 	for n in pikasp.names:
-		if n.language.name == 'en':
+		if n.language.name == 'pt':
 			pname = n.name
 	PRE_URL = 'https://bulbapedia.bulbagarden.net/wiki/'
 	MID = pname + '_%28Pokémon%29'
 	B_URL = PRE_URL+MID
 
-	ans = 'Pokémon Name: ['+pname+']('+B_URL+')' + '\n\n'
+	ans = 'Nome do Pokémon: ['+pname+']('+B_URL+')' + '\n\n'
 
 	ans = ans + Ability.get_ability(api,query)
 
 	ans = ans + Type.fetch_type(api,query) + '\n'
 
-	ans = ans + 'Earliest Learnset: ' + Learnset.get_learnset(api,query)
+	ans = ans + 'Lista de golpes: ' + Learnset.get_learnset(api,query)
 
 	return ans
